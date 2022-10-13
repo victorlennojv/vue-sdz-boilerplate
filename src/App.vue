49fed281689fcbf839b2fc293572303228141662
Victor Almeida <victor.almeida@seedz.ag>
2022-10-13 18:18:30 -0300

    save: boilerplate

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Teste</h1>
+    <s-button>Teste</s-button>
+  </div>
+</template>
+<script>
+import { SButton } from 'vue-sdz'
+
+export default {
+  components: { SButton }
+}
+</script>
+
+
