5272bc79737b196d1500b33ee58a330ebb24d019
Victor Almeida <victor.almeida@seedz.ag>
2022-10-18 10:57:05 -0300

    Boilerplate

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
-export default new Vue({ render: h => h(App) }).$mount('#app')
+import { VueCoeFeedback } from 'vue-coe-feedback'
+
+Vue.use(VueCoeFeedback, store, { delay: 3000 })
+
+export default new Vue({ store, render: h => h(App) }).$mount('#app')
